d56b92aff68cb81ced008b8b63212c4fc2318709
samarthpdrive-ux <samarthpdrive@gmail.com>
2026-07-28 11:51:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ MYSQL_DB = "telegram_shop"
 # Change this if your certificate has a different filename
 MYSQL_SSL_CA = "ca.pem"
 ADMIN_IDS = [
-    8790675033
+    7943742895
 ]
 # ==========================================================
 # DEPOSIT NETWORKS
